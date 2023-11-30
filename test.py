c51a5b2efa402d67b682e5a235b9fa66e0b45ba7
Logan Martin <lmartin27@charlottesvilleschools.org>
2023-11-30 16:51:47 -0500

    code testinging

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
+# type: ignore
+
+import board
+import digitalio
+import time
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+function testFun() {
+   print("LED should be on!")
+}
+
+time.sleep(2)
+testFun()
+
+led.value = True
+time.sleep(2)
+led.value = False
+
 
 
