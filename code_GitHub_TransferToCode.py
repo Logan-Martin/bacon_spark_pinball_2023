63ba549643c72e3283ff77744cd47c80e3195939
Logan Martin <lmartin27@charlottesvilleschools.org>
2023-12-05 09:22:07 -0500

    updated code py

--- code_GitHub_TransferToCode.py
+++ code_GitHub_TransferToCode.py
@@ -3,11 +3,19 @@
 import board
 import digitalio
 import time
+import pwmio
+from adafruit_motor import servo
+
 led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
+led.direction = digitalio.Direction.OUTPUT\
+
+pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
+servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
 
 def testFun():
    print("LED should be on!")
+   servo1.angle = 0
+
 
 time.sleep(1)
 
@@ -16,6 +24,7 @@ while True:
    testFun()
    time.sleep(1)
    led.value = False
+   servo1.angle = 90
    time.sleep(1)
 
 
