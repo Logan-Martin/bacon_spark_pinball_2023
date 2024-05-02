5ec5fa2bb578a92696658d10ff2c1a45745489ab
Logan Martin <71342159+Logan-Martin@users.noreply.github.com>
2024-05-02 16:59:00 -0400

    Update code_GitHub_TransferToCode.py

--- code_GitHub_TransferToCode.py
+++ code_GitHub_TransferToCode.py
@@ -6,54 +6,29 @@ import time
 import pwmio
 from adafruit_motor import servo
 
-button_left = digitalio.DigitalInOut(board.GP13)
-button_left.direction = digitalio.Direction.OUTPUT
+button_left = digitalio.DigitalInOut(board.GP13)  # Button stuff
+button_left.direction = digitalio.Direction.INPUT
+button_left.pull = digitalio.Pull.UP
 debounce_ButtonLeft = False
 
-button_right = digitalio.DigitalInOut(board.GP16)
-button_right.direction = digitalio.Direction.OUTPUT
-debounce_ButtonRight = False
-
-pwm_servo_left = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
-servo_left = servo.Servo(pwm_servo_left, min_pulse=500, max_pulse=2500)
-
-pwm_servo_right = pwmio.PWMOut(board.GP3, duty_cycle=2 ** 15, frequency=50)
-servo_right = servo.Servo(pwm_servo_right, min_pulse=500, max_pulse=2500)
-
-
-time.sleep(1)
+pwm_servo_left = pwmio.PWMOut(board.GP1, duty_cycle=2 ** 15, frequency=50)
+servo_left = servo.ContinuousServo(pwm_servo_left, min_pulse=500, max_pulse=1250)
 
 while True:
 
-   if button_left.value == True:
+    ##print(debounce_ButtonLeft)
+    ##print(button_left.value)
 
-      if debounce_ButtonLeft == False:
-         debounce_ButtonLeft = True
-         print("True, left!")
-         servo_left.angle = 90
+    if button_left.value == False:
+        if debounce_ButtonLeft == False:
+            debounce_ButtonLeft = True
+            print("turn 90")
+            servo_left.throttle = 0.5
 
-   else:
-
-      if debounce_ButtonLeft == True:
-         debounce_ButtonLeft = False
-         print("False, left!")
-         servo_left.angle = 0
-
-
-
-   if button_right.value == True:
-
-      if debounce_ButtonRight == False:
-         debounce_ButtonRight = True
-         print("True, right!")
-         servo_right.angle = 90
-
-   else:
-
-      if debounce_ButtonRight == True:
-         debounce_ButtonRight = False
-         print("False, right!")
-         servo_right.angle = 0
-
-
-      
+    elif button_left.value == True:
+        if debounce_ButtonLeft == True:
+            debounce_ButtonLeft = False
+            print("turn 0")
+            servo_left.throttle = 0
+            
+    time.sleep(0.1)
