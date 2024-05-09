78e0ef52b6da8e8b66f009acf350cf7f50d9ee21
Logan Martin <71342159+Logan-Martin@users.noreply.github.com>
2024-05-09 16:55:39 -0400

    Update code_GitHub_TransferToCode.py

--- code_GitHub_TransferToCode.py
+++ code_GitHub_TransferToCode.py
@@ -12,7 +12,9 @@ button_left.pull = digitalio.Pull.UP
 debounce_ButtonLeft = False
 
 pwm_servo_left = pwmio.PWMOut(board.GP1, duty_cycle=2 ** 15, frequency=50)
-servo_left = servo.ContinuousServo(pwm_servo_left, min_pulse=500, max_pulse=1250)
+servo_left = servo.ContinuousServo(pwm_servo_left, min_pulse=1000, max_pulse=2000)
+
+servo_left.throttle = 0
 
 while True:
 
@@ -22,13 +24,16 @@ while True:
     if button_left.value == False:
         if debounce_ButtonLeft == False:
             debounce_ButtonLeft = True
-            print("turn 90")
+            print("turn a")
             servo_left.throttle = 0.5
+            time.sleep(0.2)
+            servo_left.throttle = 0
 
     elif button_left.value == True:
         if debounce_ButtonLeft == True:
             debounce_ButtonLeft = False
-            print("turn 0")
+            servo_left.throttle = -0.5
+            time.sleep(0.2)
             servo_left.throttle = 0
-            
+
     time.sleep(0.1)
